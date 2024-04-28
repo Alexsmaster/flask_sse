0656c130467e240a7b6170678241a3225a2a3d74
alexmaster <alexsmaster@gmail.com>
2024-04-28 23:38:59 +0400

    small changes in html

--- sse.py
+++ sse.py
@@ -1,18 +1,30 @@
 from flask import Flask, render_template
 from flask_sse import sse
-import os
+import time
 
 app = Flask(__name__)
-app.config["REDIS_URL"] = os.environ.get("REDIS_URL")
+app.config["REDIS_URL"] = "redis://localhost"
 app.register_blueprint(sse, url_prefix='/stream')
-
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-
 @app.route('/hello')
 def publish_hello():
     sse.publish({"message": "Hello!"}, type='greeting')
     return "Message sent!"
+
+# Function to create heavy CPU load
+def heavy_task(delay=5):
+    # Simulating heavy CPU load
+    time.sleep(delay)
+    # for _ in range(int(delay)):
+    #     time.sleep(1)
+
+# Route to initiate heavy CPU load
+@app.route('/heavy_task')
+def heavy_task_route():
+    # Starting heavy task in a separate process
+    heavy_task()
+    return "Heavy task initiated."
