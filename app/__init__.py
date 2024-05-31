280f2a1385b48100dca459c346afb14130d08a64
alexandr <alexsmaster@gmail.com>
2024-05-31 04:51:31 +0400

    Works with rabbitmq

--- app/__init__.py
+++ app/__init__.py
@@ -8,30 +8,41 @@ from datetime import datetime
 # from app import announce
 # from app.announce import format_sse #announcer
 import gevent
+import gevent.queue
 from gevent.pywsgi import WSGIServer
-from gevent import monkey
-monkey.patch_all()
+# from gevent import monkey
+# monkey.patch_all()
 
-
+import pika
+import queue
 import os
+import threading
 from multiprocessing import current_process
 from threading import current_thread
 
+
 import socket
-
-
 
 
 app = Flask(__name__)
 app.logger.setLevel(logging.DEBUG)
 app.logger.debug('app created')
-log_formatter = Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s [%(threadName)s] ") # I am printing thread id here
-console_handler = StreamHandler()
-console_handler.setFormatter(log_formatter)
-app.logger.addHandler(console_handler)
-app.logger.info("Hi, how are you")
 
-file_path='messages.txt'
+
+
+
+
+#rabbitmq sender env
+# logging.basicConfig()
+url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@192.168.33.60/%2f')
+params = pika.URLParameters(url)
+params.socket_timeout = 5
+#rabbitmq sender env
+
+
+message_queue = queue.Queue()
+gevent_queue = gevent.queue.Queue()
+
 
 @app.route('/')
 def index():
@@ -41,56 +52,88 @@ def index():
 
 @app.route('/api/sse')
 def apisse():
-    def read_messages():
-        known_messages = set()
+    app.logger.debug("enter api SSE: ")
+    app.logger.debug("message_queue: " + message_queue.__repr__())
+    pid = os.getpid()
+    thread_name = current_thread().name
+    process_name = current_process().name
+    app.logger.debug(f"pid = {pid} - {process_name} - {thread_name}")
+
+
+    def events_sse():
+
+
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.33.60'))
+
+        channel = connection.channel()
+
+        channel.exchange_declare(exchange='sse_events', exchange_type='fanout')
+        result = channel.queue_declare(queue='', exclusive=True)
+        queue_name = result.method.queue
+        app.logger.debug("rabbitMQ Queue name: " + queue_name)
+        channel.queue_bind(exchange='sse_events', queue=queue_name)
+
+        # def callback(ch, method, properties, body):
+        #     yield format_sse(str(body))
+
+
+
         while True:
-            if os.path.exists(file_path):
-                with open(file_path, 'r') as f:
-                    messages = f.readlines()
+            method_frame, header_frame, body = channel.basic_get(queue_name)
+            if method_frame:
+                app.logger.debug("rabbitMQ Queue name: " + queue_name + " With: " + str(method_frame) + " ; " + str(header_frame) + " : " + str(body))
+                channel.basic_ack(method_frame.delivery_tag)
+                yield format_sse("Q:" + queue_name + str(body))
+                # channel.basic_ack(method_frame.delivery_tag)
+            else:
+                app.logger.debug("Q:" + queue_name + ' No message returned')
+                yield format_sse("Q:" + queue_name + ' No message returned')
+            # msg = gevent_queue.get(block=True, timeout=10)
+            # channel.basic_get(
+            #     queue=queue_name, callback=callback, auto_ack=True)
+            time.sleep(0.05)
 
-                for message in messages:
-                    message = message.strip()
-                    if message not in known_messages:
-                        app.logger.debug(f"New message: {message}")
-                        event_data = format_sse(data=('Py|msg: ' + str(message)),
-                                                event='message')  # json.dumps(data)
-                        yield event_data
-                        # event_data = format_sse(data=('/api/callGET ' + str(message)),
-                        #                         event='occur')  # json.dumps(data)
-                        # yield event_data
-                        known_messages.add(message)
-                time.sleep(0.01)
 
-    # def events_sse():
-    #     messages = announcer.listen()
-    #     while True:
-    #         msg = messages.get()
-    #         app.logger.debug(msg)
-    #         yield msg  # blocks until a new message arrives
     app.logger.debug('/api/sse started')
-    # return Response(events_sse(), content_type='text/event-stream')
-
     resp = Response(
-        read_messages(),
+        events_sse(),
         mimetype='text/event-stream'
     )
-    # resp.headers['X-Accel-Buffering'] = 'no'
-    # resp.headers['Cache-Control'] = 'no-cache'
+    resp.headers['X-Accel-Buffering'] = 'no'
+    resp.headers['Cache-Control'] = 'no-cache'
     return resp
 
-    #return Response(read_messages(), content_type='text/event-stream')
+
+
 
 # we recieve post request and writing data to file. responding is just echo
 @app.route('/api/call', methods=['GET', 'POST'])
 def apicall():
     if request.method == 'GET':
         return jsonify("/api/call GET: nothing to do ")
+
     elif request.method == 'POST':
         data = request.get_json()['text'] #just because text field is sent from the client
         # app.logger.debug(data)
-        with open(file_path, 'a') as f:
-            f.write(data + '\n')
+        # with open(file_path, 'a') as f:
+        #     f.write(data + '\n')
+
+
+
+        connection_sender = pika.BlockingConnection(params)  # Connect to CloudAMQP
+        channel_sender = connection_sender.channel()  # start a channel
+        # channel_sender.queue_declare(queue='pdfprocess')  # Declare a queue
+        channel_sender.exchange_declare(exchange='sse_events',
+                                 exchange_type='fanout')
+
+        # Message to send to rabbitmq
+        bodys = 'data ke ' + data #str()
+        channel_sender.basic_publish(exchange='sse_events', routing_key='', body=bodys)
+        connection_sender.close()
+
+        gevent_queue.put(data)
         return jsonify("/api/call POST", data)
+
     else:
         pass
     return jsonify("/api/call its not GET or POST request!!")
@@ -99,16 +142,20 @@ def apicall():
 
 @app.route('/api/cpubound')
 def cpubound():
-    # i7-8700k deals with it in  12.82s with debug
+    # i7-8700k deals with it in  12.82s with debug range(1000, 16000)
     start_time = time.perf_counter()
     for num in range(1000, 16000):
-    #     get_prime_numbers(num)
-    # for num in range(1000, 16000):
+        get_prime_numbers(num)
+    for num in range(1000, 16000):
+        get_prime_numbers(num)
+    for num in range(1000, 16000):
         get_prime_numbers(num)
     end_time = time.perf_counter()
 
     app.logger.debug(f"api/cpubound : Elapsed run time: {end_time - start_time} seconds")
-    return '', 204
+    # with open(file_path, 'a') as f:
+    #     f.write(f"api/cpubound : Elapsed run time: {end_time - start_time} seconds" + '\n')
+    return render_template('cpubound.html', timedelta=end_time - start_time)
 
 
 # @app.route('/download')
--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -7,14 +7,12 @@ from glob import glob
 
 
 # TIMEOUT = 5
-preload = True
-TIMEOUT = 0
+# preload = True
+TIMEOUT = 600
 bind = "0.0.0.0:5000"
 # bind = "127.0.0.1:5000"
 # backlog = 2048
 workers = 15
-
-
 #threads = 4
 # max_requests = 3000
 # max_requests_jitter = 100
@@ -22,9 +20,9 @@ reload = True
 reload_extra_files = glob('app/**/*.html', recursive=True) + glob('app/**/*.css', recursive=True) + glob('app/**/*.js', recursive=True)#["app/templates/index.html", "app/static/sse.js"]
 #worker_class = 'sync'
 worker_class = 'gevent'
-# worker_connections = 3000
+worker_connections = 1000
 # timeout = 3000
-# keepalive = 3000
+keepalive = 3000
 spew = False
 #check_config = True
 
@@ -34,65 +32,40 @@ accesslog = '-'
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 logfile = 'log.txt'
 
-#
-#   post_fork - Called just after a worker has been forked.
-#
-#       A callable that takes a server and worker instance
-proc_name = None
+
+proc_name = 'worker'
 
 
 def post_fork(server, worker):
     server.log.info("Worker spawned (pid: %s)", worker.pid)
 
+
 def pre_fork(server, worker):
     pass
+#
+# def pre_exec(server):
+#     server.log.info("Forked child, re-executing.")
+#
+# def when_ready(server):
+#     server.log.info("Server is ready. Spawning workers")
+#
+# def worker_int(worker):
+#     worker.log.info("worker received INT or QUIT signal")
+#
+#     ## get traceback info
+#     import threading, sys, traceback
+#     id2name = {th.ident: th.name for th in threading.enumerate()}
+#     code = []
+#     for threadId, stack in sys._current_frames().items():
+#         code.append("\n# Thread: %s(%d)" % (id2name.get(threadId,""),
+#             threadId))
+#         for filename, lineno, name, line in traceback.extract_stack(stack):
+#             code.append('File: "%s", line %d, in %s' % (filename,
+#                 lineno, name))
+#             if line:
+#                 code.append("  %s" % (line.strip()))
+#     worker.log.debug("\n".join(code))
+#
+# def worker_abort(worker):
+#     worker.log.info("worker received SIGABRT signal")
 
-def pre_exec(server):
-    server.log.info("Forked child, re-executing.")
-
-def when_ready(server):
-    server.log.info("Server is ready. Spawning workers")
-
-def worker_int(worker):
-    worker.log.info("worker received INT or QUIT signal")
-
-    ## get traceback info
-    import threading, sys, traceback
-    id2name = {th.ident: th.name for th in threading.enumerate()}
-    code = []
-    for threadId, stack in sys._current_frames().items():
-        code.append("\n# Thread: %s(%d)" % (id2name.get(threadId,""),
-            threadId))
-        for filename, lineno, name, line in traceback.extract_stack(stack):
-            code.append('File: "%s", line %d, in %s' % (filename,
-                lineno, name))
-            if line:
-                code.append("  %s" % (line.strip()))
-    worker.log.debug("\n".join(code))
-
-def worker_abort(worker):
-    worker.log.info("worker received SIGABRT signal")
-
-def ssl_context(conf, default_ssl_context_factory):
-    import ssl
-
-    # The default SSLContext returned by the factory function is initialized
-    # with the TLS parameters from config, including TLS certificates and other
-    # parameters.
-    context = default_ssl_context_factory()
-
-    # The SSLContext can be further customized, for example by enforcing
-    # minimum TLS version.
-    context.minimum_version = ssl.TLSVersion.TLSv1_3
-
-    # Server can also return different server certificate depending which
-    # hostname the client uses. Requires Python 3.7 or later.
-    def sni_callback(socket, server_hostname, context):
-        if server_hostname == "foo.127.0.0.1.nip.io":
-            new_context = default_ssl_context_factory()
-            new_context.load_cert_chain(certfile="foo.pem", keyfile="foo-key.pem")
-            socket.context = new_context
-
-    context.sni_callback = sni_callback
-
-    return context
